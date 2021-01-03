b455f8da00eb38f8b0dbc7f1d1fa6baecafbb4b2
ashtime99 <ashtime99@gmail.com>
2021-01-03 22:11:24 +0800

    first

---
+++ src/main/java/com/xyc/hungry/HungryApplication.java
@@ -0,0 +1,13 @@
+package com.xyc.hungry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HungryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HungryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xyc/hungry/HungryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyc.hungry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HungryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
